763db6ecf04f180cf559b5365ccb9b52bb95d0c0
FayzullaevUmid707 <dilnoraurazova7@gmail.com>
2024-07-02 19:23:34 +0500

    initial upload

---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './Navbar/navbar'
+import Footer from './Footer/footer'
+import Section from './Section/section'
+
+const page = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Section/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default page
